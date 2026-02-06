16038e0e01824cad2ab374633877b786ff472fbd
AdityaVarma45 <maheahadityavarma2327@gmail.com>
2026-02-06 19:13:44 +0530

    updating cors

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,13 @@ import authRoutes from "./routes/auth.js";
 import notesRoutes from "./routes/notes.js";
 import path from "path";
 import cors from "cors";
-app.use(cors());
+
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", authRoutes);
